0a5e50c7b35754e4cb57be682a1bcfc37a1008c2
API <fudongyi@APIdeMacBook-Pro.local>
2019-08-18 14:31:51 +0800

    123

--- demo/settings.py
+++ demo/settings.py
@@ -1,7 +1,7 @@
 class DebugConfig(object):
     SECERT_KEY = "79879d7f89ds7f"
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///blog.db"
+    SQLALCHEMY_DATABASE_URI = r"sqlite:////Users/fudongyi/PycharmProjects/flask_demo2/blog.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SALT = "adhfuiywfasiuk"
     CHARSET = "utf-8"
